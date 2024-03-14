80f950d109a0e27a8f94099260b70ec059d37bce
EdharKruk <s26827@pjwstk.edu.pl>
2024-03-14 22:23:15 +0300

    change variable sum on sumTotal

--- obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ff3627c3949d34628be5fcf06272f195014740")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f4a10aec6a13b7c5ac5dc72433eba74b28afa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
